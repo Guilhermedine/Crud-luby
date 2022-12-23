43d64e4fb4be10992350d9a16b630083d3033745
Guilherme Dine <guilherme.dine@gmail.com>
2022-12-23 11:22:04 -0300

    API

--- sequelize.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  username: "docker",
-  password: "docker",
-  database: "crud",
-  host: "localhost",
-  dialect: "postgres",
-  port: "5432"
-}
---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: 'localhost',
+  username: 'docker',
+  password: 'docker',
+  database: 'crud',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
