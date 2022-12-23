9b9a509ff7907ace623178eb69e4edcc12944023
Guilherme Dine <guilherme.dine@gmail.com>
2022-12-23 13:25:12 -0300

    API

--- src/database/migrations/20221223141547-create-users.js
+++ src/database/migrations/20221223141547-create-users.js
@@ -1,9 +1,47 @@
 'use strict';
+require('bcrypt')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up(queryInterface, Sequelize) {
-    return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+  up(queryInterface, S) {
+    return queryInterface.createTable('users', {
+      id: {
+        type: S.UUID,
+        defaultValue: S.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      cpf: {
+        type: S.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: S.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: S.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password_hash: {
+        type: S.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: S.DATE,
+        allowNull: false,
+
+      },
+      updated_at: {
+        type: S.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: S.DATE,
+        allowNull: false,
+      }
+    });
 
   },
 
