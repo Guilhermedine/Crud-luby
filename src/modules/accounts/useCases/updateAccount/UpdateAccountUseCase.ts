936100b893372f0a036d5cd6541e201d3cf94884
Guilherme Dine <guilherme.dine@gmail.com>
2023-01-04 13:07:10 -0300

    CRUD

--- src/modules/accounts/useCases/updateAccount/UpdateAccountUseCase.ts
+++ src/modules/accounts/useCases/updateAccount/UpdateAccountUseCase.ts
@@ -2,7 +2,7 @@ import { injectable, inject } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { AppError } from "../../../../errors/AppError"
 import { User } from "../../../../database/models/User";
-import { compare } from "bcrypt"
+import { where } from "sequelize";
 
 interface IRequest {
   name,
@@ -39,6 +39,14 @@ class UpdateAccountUseCase {
     if (emailAlreadyInUse) {
       throw new AppError("Email already in use")
     }
+
+    user.update({
+      name,
+      avatar,
+      biography,
+      email,
+      password_hash
+    });
 
     return {
       name,
