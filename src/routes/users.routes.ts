4fd07e1b7fd4de4f2641f4932fa5f15c75335a84
Guilherme Dine <guilherme.dine@gmail.com>
2023-01-05 15:59:53 -0300

    CRUD

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-
 
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController"
 
