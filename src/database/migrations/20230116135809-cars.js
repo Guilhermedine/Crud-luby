f0dae5b653f5a09ae370c8aa794c93a562bca39b
Guilherme Dine <guilherme.dine@gmail.com>
2023-01-16 12:20:49 -0300

    CRUD

---
+++ src/database/migrations/20230116135809-cars.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return await queryInterface.createTable('cars', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        unique: true
+      },
+      brand: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      model: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      year: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      km: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      color: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      chassis: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      purchase_price: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      solded_by: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('now()'),
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('now()'),
+        allowNull: false,
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return await queryInterface.dropTable('cars');
+  }
+};
---
+++ src/database/models/Cars.ts
@@ -0,0 +1,55 @@
+import { sequelize } from '../../config/database';
+import { Model, DataTypes } from 'sequelize';
+
+
+
+class Cars extends Model {
+}
+
+Cars.init({
+  id: {
+    type: DataTypes.UUIDV4,
+    autoIncrement: true,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV1,
+  },
+  brand: {
+    type: DataTypes.STRING
+  },
+  model: {
+    type: DataTypes.STRING
+  },
+  year: {
+    type: DataTypes.STRING
+  },
+  km: {
+    type: DataTypes.STRING
+  },
+  color: {
+    type: DataTypes.STRING
+  },
+  chassis: {
+    type: DataTypes.STRING
+  },
+  purchase_price: {
+    type: DataTypes.STRING
+  },
+  status: {
+    type: DataTypes.STRING
+  },
+  solded_by: {
+    type: DataTypes.STRING
+  },
+  created_at: {
+    type: DataTypes.DATE,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+  }
+}, {
+  tableName: "cars",
+  timestamps: true,
+  createdAt: "created_at",
+  updatedAt: "updated_at",
+  sequelize: sequelize
+});
