3b595a1ad82a3828022078e082459a661dcb6c5f
Guilherme Dine <guilherme.dine@gmail.com>
2023-01-17 16:38:31 -0300

    refactor: changed name from LocateCar to ShowCar

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { LocateCarController } from "../modules/cars/useCases/locateCar/LocateCarController";
+import { ShowCarController } from "../modules/cars/useCases/showCar/ShowCarController";
 import { AddCarController } from "../modules/cars/useCases/addCar/AddCarController";
 import { ListAllCarsController } from "../modules/cars/useCases/listAllCars/ListAllCarsController"
 import { DeleteCarController } from "../modules/cars/useCases/deleteCar/DeleteCarController";
+import { SellCarController } from "../modules/cars/useCases/sellCar/SellCarController"
 
 const carsRoutes = Router();
 
@@ -10,16 +11,20 @@ const addCarController = new AddCarController();
 
 const listAllCarsController = new ListAllCarsController();
 
-const locateCarController = new LocateCarController();
+const showCarController = new ShowCarController();
 
 const deleteCarController = new DeleteCarController();
+
+const sellCarController = new SellCarController();
 
 carsRoutes.post("/", addCarController.handle)
 
 carsRoutes.get("/", listAllCarsController.handle)
 
-carsRoutes.get("/:id", locateCarController.handle)
+carsRoutes.get("/:id", showCarController.handle)
 
 carsRoutes.delete("/:id", deleteCarController.handle)
+
+carsRoutes.put("/:id", sellCarController.handle)
 
 export { carsRoutes }
